931786ae9cd02bbd48e63719ca5886bb08186ffa
Ramayan Prasad <ram.developer89@gmail.com>
2019-05-16 20:37:40 +0530

    Laravel Contact Form

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -5,7 +5,7 @@
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-          <title>Contact Us Test</title>
+          <title>Contact Us Form</title>
     </head>
         <body>
 
@@ -16,7 +16,7 @@
                 </div>
             @endif
 
-          <h3>Contact Us Test</h3>
+          <h3>Contact Us Form</h3>
 
           <form action="{{route('contact')}}" method="POST">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
